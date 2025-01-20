830a7847a7919476aacf032e47db2dd49e5fd04b
kamalabdi8 <kamalabdi042@gmail.com>
2025-01-20 11:52:24 +0300

    initial commit

--- server/models.py
+++ server/models.py
@@ -7,3 +7,6 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)  # Added name column
+    image = db.Column(db.String, nullable=False)  # Added image column
+    price = db.Column(db.Numeric(10, 2), nullable=False)  # Added price column
